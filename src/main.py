82b3c579f76f986ae820e7fd2b0e80cb99da22c9
MarvelousChester <karandeepsandhu2789@gmail.com>
2024-02-21 20:17:10 -0500

    Fix bugs with writing to file
    - add \n to end of log record
    - remove old message function

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ import socket
 import sys
 import json
 
-def formatMessage1(record):
+def formatMessage(record):
     
     
     decoded_record = record.decode("utf-8")
@@ -21,9 +21,8 @@ def formatMessage1(record):
         print(counter)
 
     formatted_message = dataConfig["formatted_message"].format(**dataConfig["format"])
-    print(formatted_message)
-     
-        
+    
+    formatted_message += "\n"
         
         
     # how many formats given
@@ -45,32 +44,8 @@ def formatMessage1(record):
     
    # print(formatted_message)
 
-    return "formatted_message"
-
-    
-# Have to use JSON for the information
-def formatMessage(record):
-    
-    
-    decoded_record = record.decode("utf-8")
-    new_record_list = decoded_record.split("|!|")
-
-    # PUT THE CONTENT IN ORDER OF THE FORMAT 
-    
-    level = new_record_list[0]
-    ip = new_record_list[1]
-    date_time = new_record_list[2] 
-    time_zone = new_record_list[3]
-    content = new_record_list[4]
-    
-    
-   #formatted_message = f"[{level}] {ip} {date_time} {time_zone} - {content}\n"
-    
-    formatted_message = f"[{level}] [{ip}] [{date_time} {time_zone}] - {content}\n"
-    
-    print(formatted_message)
-
     return formatted_message
+
  
 def writeIntoLog(log_path, record):
     
