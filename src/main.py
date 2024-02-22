8be31dd5722ac5028774ddfb8e14de6a30905127
MarvelousChester <karandeepsandhu2789@gmail.com>
2024-02-22 16:13:12 -0500

    Add multi-threading to allow multiple client

--- src/main.py
+++ src/main.py
@@ -1,41 +1,45 @@
+import asyncio
 import socket
 import sys
 import json
+import aiofiles
 
 
+import threading
 
-def formatMessage(record):
+def formatMessage(record, format_config):
     
     decoded_record = record.decode("unicode_escape")
-    config_file = open("config.json")
-    data_config = json.load(config_file)
     record_in_json = json.loads(decoded_record)
 
     
-    if(data_config["format"].keys() == record_in_json.keys()):
+    if(format_config["format"].keys() == record_in_json.keys()):
         print("same")
-        formatted_message = dataConfig["formatted_message"].format(**record_in_json)
-
+        formatted_message = format_config["formatted_message"].format(**record_in_json)
         formatted_message += "\n"   
     else:
         print("not same")
         # DO I Send back message?
         formatted_message = "Invalid Log Record Given\n"
  
-
     return formatted_message
  #https://pypi.org/project/pyrate-limiter/
-def writeIntoLog(log_path, record):
-    
-    # When logging get instance id and attach that to log file name and then just date
-    try:        
-        with open(log_path, 'a') as file:
-            formatted_message = formatMessage(record)
-            file.write(formatted_message)
-    except IOError:
-        print("ERROR: record could not be written to file")
-        # Send a message back that something went wrong 
+
+lock = threading.Lock()
             
+def writeIntoLogWorker(record, format_config):
+   
+    log_location = format_config["log_service_config"]["log_location"]
+    if record:
+        lock.acquire()
+        try:        
+            with open(log_location, 'a') as file:
+                formatted_message = formatMessage(record, format_config)
+                file.write(formatted_message)
+                # Should add a timeout in case wait takes too long then skip to stop hanging of service
+                lock.release()
+        except IOError:
+            print("ERROR: record could not be written to file")
 
 
 config_file = open("config.json")
@@ -50,6 +54,8 @@ log_location = dataConfig["log_service_config"]["log_location"]
 
 
 
+# JUST USE THREADs
+
 # setting up listener
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -59,19 +65,26 @@ address = (service_ip, int(service_port))
 sock.bind(address)
 
 sock.listen()
-while True:
-        print("waiting for connection")
-        conn, addr = sock.accept()
-        print("Connected")
+
+while (True):
+    
+    print("waiting for connection")
+    conn, addr = sock.accept()
+    print("Connected")
+    try:
+        record = conn.recv(2054)
+        x = threading.Thread(target=writeIntoLogWorker, args=(record, dataConfig,))
+        x.start()
+    except Exception as e:
+        print(f"Error with connection :{e}")
+    finally:
+        conn.close()
+
+        # accept connection
+        # shoot of an async worker that will be able to write into file
+        # then write into
         
-        data = conn.recv(2054)
-        if data:
-            writeIntoLog(log_location, data)
+        
+# Could have it so that each time a writer is going and it's done it will decrement and incr 
+        
                   
-
-
-
-
-
-
-
